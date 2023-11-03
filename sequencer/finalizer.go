5effac6cd84a05c9e8992d2a80716d71135f46aa
BananaLF <864685021@qq.com>
2023-11-03 16:57:21 +0800

    update f.lastGERHash

--- sequencer/finalizer.go
+++ sequencer/finalizer.go
@@ -1015,6 +1015,8 @@ func (f *finalizer) syncWithState(ctx context.Context, lastBatchNum *uint64) err
 			return fmt.Errorf("failed to get work-in-progress batch, err: %w", err)
 		}
 	}
+
+	f.lastGERHash = f.batch.globalExitRoot
 	log.Infof("Initial Batch: %+v", f.batch)
 	log.Infof("Initial Batch.StateRoot: %s", f.batch.stateRoot.String())
 	log.Infof("Initial Batch.GER: %s", f.batch.globalExitRoot.String())
